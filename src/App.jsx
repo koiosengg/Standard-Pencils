90fd11cef2b1b2391b7311908b22f7414beb04dd
Panth1823 <panthshah242@gmail.com>
2026-05-10 23:02:45 +0530

    only homepage live , implement WorkInProgress component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import AboutUs from "./components/AboutUs";
-import CapabilitiesPage from "./components/CapabilitiesPage";
-import Product from "./components/Product";
-
+import WorkInProgress from "./components/WorkInProgress";
 
 function App() {
   return (
@@ -18,11 +14,7 @@ function App() {
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<Home />} />
-          <Route path="about" element={<AboutUs />} />
-          <Route path="capabilities" element={<CapabilitiesPage />} />
-          <Route path="product" element={<Product />} />
+          <Route path="*" element={<WorkInProgress />} />
         </Route>
       </Routes>
       <Footer />
---
+++ src/components/WorkInProgress.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function WorkInProgress() {
+  return (
+    <div className="wip-container">
+      <div className="wip-content">
+        <h1>🚧 Work in Progress</h1>
+        <p>We're still building this page. Check back soon!</p>
+        <a href="/" className="wip-home-link">Back to Home</a>
+      </div>
+    </div>
+  );
+}
+
+export default WorkInProgress;
